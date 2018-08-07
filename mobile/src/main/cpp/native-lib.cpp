241b9ff4e9fe69047f26c9f46f3703099826af3d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-08-07 18:22:21 +0900

    first commit

---
+++ mobile/src/main/cpp/native-lib.cpp
@@ -0,0 +1,11 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_kobayashi_taku_taptappun_net_rstack2018_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
