b7605ef2736e7269c217d83c6c38def5e069d382
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-08-08 12:44:01 +0900

    Wearableで心拍数が取れるらしい

---
+++ wear/src/main/java/kobayashi/taku/taptappun/net/rstack2018/Config.java
@@ -0,0 +1,6 @@
+package kobayashi.taku.taptappun.net.rstack2018;
+
+public class Config {
+   public static final String TAG = "rstack2018";
+   public static final String ROOT_URL = "https://knoget8t9e.execute-api.ap-northeast-1.amazonaws.com";
+}
--- wear/src/main/java/kobayashi/taku/taptappun/net/rstack2018/MainActivity.java
+++ wear/src/main/java/kobayashi/taku/taptappun/net/rstack2018/MainActivity.java
@@ -1,12 +1,20 @@
 package kobayashi.taku.taptappun.net.rstack2018;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends WearableActivity {
+import java.util.List;
+
+public class MainActivity extends WearableActivity implements SensorEventListener{
 
     private TextView mTextView;
+    private SensorManager mSensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +23,48 @@ public class MainActivity extends WearableActivity {
 
         mTextView = (TextView) findViewById(R.id.text);
 
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
         // Enables Always-on
         setAmbientEnabled();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(Config.TAG, "Resume");
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE), SensorManager.SENSOR_DELAY_NORMAL);
+        //mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d(Config.TAG, "Sensor");
+        if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
+            for(float heart : event.values){
+                Log.d(Config.TAG, String.valueOf(heart));
+                //心拍数を表示
+                mTextView.setText(String.valueOf(heart));
+            }
+        }
+        /*
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            Log.d(Config.TAG, "acceler");
+            for(float heart : event.values){
+                Log.d(Config.TAG, String.valueOf(heart));
+            }
+        }
+        */
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(Config.TAG, "ac");
     }
 }
